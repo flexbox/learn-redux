6c1bd64f14ec206f93aab6afc03a49652d243ae3
David Leuliette <david.leuliette@smartpension.co.uk>
2017-09-18 23:08:43 +0100

    Integrate React Router to handle page changes into our React app

--- client/components/Main.js
+++ client/components/Main.js
@@ -6,10 +6,10 @@ const Main = React.createClass({
     return (
       <div>
         <h1>
-          <Link to="/">ReduxStagram</Link>
+          <Link to="/">Reduxstagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
-      {React.cloneElement(this.props.children, this.props)}
     )
   }
 });
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,6 +5,20 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // Import Components
-import Main from './components/Main'
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
