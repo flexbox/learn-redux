858584cd9e8c627280bd18e4276c01b46d60927b
David Leuliette <dleuliette@gmail.com>
2017-09-19 23:10:14 +0100

    Add reducers for handle the updating of our state in our store

---
+++ client/reducers/posts.js
@@ -0,0 +1,11 @@
+// a reducer takes in two things:
+
+// 1. the action (info about waht happened)
+// 2. copy of current state
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
