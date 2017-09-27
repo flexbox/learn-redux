5a553174a56ab33c560c1c5ff5151e28f7fe0f71
David Leuliette <dleuliette@gmail.com>
2017-09-27 21:38:44 +0100

    Updating Comment State in our Store

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  handleSubmit(e){
+    e.preventDefault();
+    const { postId } = this.props.params.postId;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+  },
   renderComment(comment, i){
     return(
       <div className="comment" key={i}>
@@ -13,11 +20,10 @@ const Comments = React.createClass({
     )
   },
   render() {
-
     return (
       <div className="comment">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
